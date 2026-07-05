106450e2eebd84e48b6c82cc7f7e5e10dc44f6e5
AlonsoMD <alonso2295@hotmail.com>
2026-07-05 14:10:23 -0500

    Removed sesion_10 folder

--- sesion_10/retail_workflow/src/Yelp_Project/transformations/bronze/review.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REFRESH MATERIALIZED VIEW dbassociate.${bronze_schema}.b_review
-AS
-SELECT
-    *
-FROM read_files(
-    "/Volumes/dbassociate/default/vol_landing/sesion_08/yelp_academic_dataset_review.json",
-    format => "json"
-)
-;
